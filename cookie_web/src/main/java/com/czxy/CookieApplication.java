dd0a7d1a18940961d375952eaad132fecb29cdf4
zhangli <zhangli@163.com>
2019-07-26 11:09:29 +0800

    提交哈哈哈哈

---
+++ cookie_web/src/main/java/com/czxy/CookieApplication.java
@@ -0,0 +1,16 @@
+package com.czxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author dell
+ * @version v 1.0
+ * @date 2019/7/25
+ */
+@SpringBootApplication
+public class CookieApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CookieApplication.class,args);
+    }
+}

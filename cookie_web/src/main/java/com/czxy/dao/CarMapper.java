5fe1e453fe09be6f02893cb8950aa008f6b87822
zhangli <zhangli@163.com>
2019-07-26 09:51:29 +0800

    ttdffdsfdsdsffdsfds

---
+++ cookie_web/src/main/java/com/czxy/dao/CarMapper.java
@@ -0,0 +1,13 @@
+package com.czxy.dao;
+
+import com.czxy.domain.Car;
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * @author dell
+ * @version v 1.0
+ * @date 2019/7/25
+ */
+@org.apache.ibatis.annotations.Mapper
+public interface CarMapper extends Mapper<Car> {
+}
